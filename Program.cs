42f7cc158a6160b64e4b6d7d3d326560d89627c4
Exilic <tobias.engberg@exilic.se>
2021-10-16 23:22:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TextTools
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string textualContent = Console.ReadLine();
+            string transformedText = textualContent
+                .Fractions()
+                .Dashes()
+                .PostalNumber();
+            Console.WriteLine(transformedText);
+            Console.ReadKey();
+        }
+
+        public static string Fractions(this string text)
+        {
+            return text
+                .Replace("1/4", "\u00bc")
+                .Replace("1/2", "\u00bd")
+                .Replace("3/4", "\u00be");
+        }
+
+        public static string Dashes(this string text)
+        {
+            return text
+                .Replace("---", "\u200a\u2014\u200a")           // em dash + hair space
+                .Replace("--", "\u2013");                       // en dash
+        }
+
+        public static string PostalNumber(this string text)
+        {            
+            return new Regex(@"\b[0 - 9]{ 7}![0 - 9]")
+                .Replace(text, m => m.Value.Insert(2, " "));                     
+        }
+    }
+}
